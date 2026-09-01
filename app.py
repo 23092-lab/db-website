264ee29b80a013cb661c5583b5ce5eccbbfb530c
Ethan Li-Zhang <23092@wc.school.nz>
2026-09-02 09:34:18 +1200

    database

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import sqlite3
+from sqlite3 import Error
 
+DATABASE = ""
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_index():  # put application's code here
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
